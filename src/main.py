81bbb916a46df5d8d6d41a251dfdda0f4e787797
Thomas Brunner <102862769+brunner-th@users.noreply.github.com>
2024-06-07 12:27:28 +0200

    added mesh loading

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+<<<<<<< Updated upstream
+=======
+import os
+>>>>>>> Stashed changes
 import DataContainers.Mesh as Mesh
 
+<<<<<<< Updated upstream
 #import SimulationCase
 
 
@@ -9,6 +14,16 @@ max_volume = 10
 
 mesh = Mesh.Mesh()
 mesh.initialize_basic_square_mesh()
+=======
+cwd = os.getcwd()
+alpha = -1
+beta = 8
+h = 0.1 #0.25, 0.1, 0.05
+
+mesh = Mesh.Mesh()
+#mesh.generate_basic_square_mesh(max_vol=h*h/2, min_angle=30)
+mesh.loadMesh(cwd+"/src/Meshes/sq_mesh_h_"+str(h).replace(".","")+".hdf5")
+>>>>>>> Stashed changes
 mesh.plot_mesh()
 
 
